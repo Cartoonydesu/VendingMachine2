d0eb15e34929ed9b8f59add53e9d631994a5dac2
Piraya Sutthiparinyanon <toon.pi45@gmail.com>
2020-11-16 13:41:35 +0700

    first version

---
+++ src/vendingmachinetest/Machine.java
@@ -0,0 +1,44 @@
+
+package vendingmachinetest;
+
+public class Machine {
+    private final Drink machine[];
+    private int index=0;
+    private Money mon;
+    
+    public Machine(int size){
+        machine = new Drink[size<=0 ? 12 : size];
+    }    
+    public void addDrink(Drink drink){
+        if(index<machine.length){
+            machine[index] = drink;
+            index++;
+        }
+    }    
+    public int getNumDrink(int select){
+        return machine[select].getNum();
+    }    
+    public int getCostDrink(int select){
+        return machine[select].getCost();
+    }
+    public String getNameDrink(int select){
+        return machine[select].getName();
+    }
+    public void tookOut(int select){
+        machine[select].tookOut();
+    }
+    public void check(int money,int select){
+        mon = new Money(money,select,machine);
+    }
+    
+    
+    @Override
+    public String toString() {
+        System.out.println("Allow drink in machine:");
+        for (int i = 0; i < index; i++) {
+            System.out.println(i + ": " + machine[i]);
+        }
+        return "";
+    }
+    
+}
